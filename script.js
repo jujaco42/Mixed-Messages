e3a5ee5948cea8860decccf6d261408d5c981bf0
Justin Conn <justinconn20@gmail.com>
2021-01-28 08:16:19 -0600

    final script file

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log('Jerry Seinfeld is king');
+
+function generateRandomNumber(num) {
+    //use array length to generate random array index
+    return Math.floor(Math.random() * num);
+}
+
+const messageObject = {
+    favoriteColor: ['red','orange', 'yellow',
+                'green', 'blue','indigo','violet'],
+    favoriteMeal: ['cheeseburger and french fries', 
+                'hot chicken breakfast biscuit', 
+                'pork belly noodle bowl',
+                'carnitas tacos',
+                'Chicken salad sandwich and an apple'],
+    favoriteSeason: ['Spring','Summer', 'Autumn', 'Winter']
+};
+
+// store message in an array
+let messageArray = [];
+
+for (let prop in messageObject) {
+    //send length of array in object to generateRandom number
+    let objectIndex = generateRandomNumber(messageObject[prop].length);
+    
+    //select the array element to be added to messageArray
+    switch(prop) {
+        case 'favoriteColor':
+            messageArray.push(`Your favorite color is: ${messageObject[prop][objectIndex]}`);
+            break
+        case 'favoriteMeal':
+            messageArray.push(`Your favorite meal is: ${messageObject[prop][objectIndex]}`);
+            break
+        case 'favoriteSeason':
+            messageArray.push(`Your favoirite season is: ${messageObject[prop][objectIndex]}`);
+            break
+        default:
+            messageArray.push('unidentified prop');
+    }
+}
+
+function formatMessage(array) {
+    //format message so array elements are each displayed on a new line
+    return array.join('\n');
+}
+
+let display = formatMessage(messageArray)
+console.log(display);
